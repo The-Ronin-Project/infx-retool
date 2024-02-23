ec28257490e95a66ba0561719a1816ed024d2b5d
Theresa Aguilar <theresa@projectronin.com>
2024-02-23 19:58:14 +0000

    code
    Change load_depends_on_data to SqlUnified

--- apps/Mapping App CMv5/General Mapper 2.0 CMv5/lib/load_depends_on_data.sql
+++ apps/Mapping App CMv5/General Mapper 2.0 CMv5/lib/load_depends_on_data.sql
@@ -5,4 +5,4 @@ select json_build_object(
   'depends_on_value_jsonb', depends_on_value_jsonb, 
   'depends_on_display', depends_on_display) 
   AS json_result  from custom_terminologies.code_depends_on
-where uuid = {{mapping_queue.selectedRow.data[0].custom_terminology_uuid}}
+where uuid = {{mapping_queue.selectedRow.data[0].custom_terminology_code_uuid}}
