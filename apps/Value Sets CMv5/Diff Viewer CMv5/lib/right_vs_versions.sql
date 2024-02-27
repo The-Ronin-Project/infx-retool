c4d182c59b4e8d3aaf926fef98487800951d41f2
Hao Sun <hao@projectronin.com>
2024-02-27 20:30:01 +0000

    Protecting app Diff Viewer CMv5

---
+++ apps/Value Sets CMv5/Diff Viewer CMv5/lib/right_vs_versions.sql
@@ -0,0 +1,5 @@
+select vs.uuid vs_uuid, vsv.uuid vsv_uuid, * from value_sets.value_set_version vsv
+join value_sets.value_set vs
+on vs.uuid = vsv.value_set_uuid
+where vs.name = {{right_vs_name.value}}
+order by version desc
