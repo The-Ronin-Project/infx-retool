965a7a936ed6617baec1fbe8f103c4844ca24356
Theresa Aguilar <theresa@projectronin.com>
2024-02-29 15:13:50 +0000

    NLP, Automap, Unknown no longer assignees
    Change get_users to SqlUnified

--- apps/Mapping App CMv5/Make Assignments CMv5/lib/get_users.sql
+++ apps/Mapping App CMv5/Make Assignments CMv5/lib/get_users.sql
@@ -1,3 +1,7 @@
 select * from project_management.user
-where first_name not in ('unknown')
+where uuid not in (
+  '70b5405d-b2ab-481b-85a5-d5b305164851',
+  '8990714d-8eeb-4acf-a5b7-abf92007a53a',
+  '8abf5747-121b-48e0-9edf-dbd820d397fb'
+)
 order by first_name
